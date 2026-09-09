7293e5598efeaa1777f9c146a5686644d71dab39
modeco80 <lily.modeco80@protonmail.ch>
2026-09-08 21:21:24 -0400

    Add some more eInputState values we will use

--- src/freecam/bxapi/include/bx/input.h
+++ src/freecam/bxapi/include/bx/input.h
@@ -6,7 +6,10 @@
 enum eInputState {
 	eInputState_CameraActivate = 0x2f,
 	// Will be reused as "put rider at position" input at a later date
-	eInputState_EditorHelp = 0x44
+	eInputState_EditorHelp = 0x44,
+	eInputState_EditorZoomIn = 0x4b,
+	eInputState_EditorZoomOut = 0x4c,
+	eInputState_VoipOnOff = 0xc8
 };
 
 class cInput {
